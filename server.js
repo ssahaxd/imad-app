caf52e8f0268a0d7d1a7ad57e34c07b7d0adc07d
Sandip Saha <sandipsmoto@gmail.com>
2017-09-02 17:24:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,8 +83,9 @@ function creatTemplate(data){
 
 /* Article Handeling */
 // Article one
-app.get('/article-one', function(req, res){
-  res.send(creatTemplate(articles['article-one']));
+app.get('/:articleName', function(req, res){
+    var articleName = req.param.articleName;
+  res.send(creatTemplate(articles['articleName']));
 });
 
 
