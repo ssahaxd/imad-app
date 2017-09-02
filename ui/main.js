b9531d349641ec29dd53e6551a9394ecb8917241
Sandip Saha <sandipsmoto@gmail.com>
2017-09-03 02:14:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick = function(){
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
-        if(request.readystate === XMLHttprequest.DONE){
+        if(request.readystate === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById("countr-badg");
