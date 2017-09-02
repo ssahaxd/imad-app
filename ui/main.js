100ec8a8eb800b2512e8657382a5538979231577
Sandip Saha <sandipsmoto@gmail.com>
2017-09-02 20:10:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,24 @@ function moveRight (){
 jet.onclick = function(){
     var interval = setInterval(moveRight, 25);
 };
+
+/* Counter Code */
+var button = document.getElementById("button");
+
+button.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttprequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+
+    request.open('GET', 'http://sandipsmoto.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
