2f553bb7635be00b5157c574d1988bbbb63f282d
Sandip Saha <sandipsmoto@gmail.com>
2017-09-03 02:26:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,12 +20,10 @@ button.onclick = function(){
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            if(request.status === 200){
+        if(request.readystate === XMLHttpRequest.DONE && request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById("countr-badge");
                 span.innerHTML = counter.toString();
-            }
         }
     };
 
