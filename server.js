7736defdba8e3fed3b3c49de950203369e7e9d94
Sandip Saha <sandipsmoto@gmail.com>
2017-09-02 17:18:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,6 @@ function creatTemplate(data){
             <title>${data.title}}</title>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width initial-scale = 1">
-            <link rel="shortcut icon" href="#">
             <link href="/ui/style.css" rel="stylesheet" />
         </head>
         <body>
