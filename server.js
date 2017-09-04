b161a943d878e2cb6ab873e544cd3f9df166bec8
Sandip Saha <sandipsmoto@gmail.com>
2017-09-04 21:04:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/counter', function(req, res) {
 
 /* Article Handeling */
 app.get('/:articleName', function(req, res){
-    pool.query("SELECT * FROM articles WHERE title = $1" + [req.params.articleName] , function (err, result){
+    pool.query('SELECT * FROM articles WHERE title = $1' + [req.params.articleName] , function (err, result){
         if(err){
             res.status(500).send(err.toString());    
         }
