28b3521c64845827fb2a3c7709343d4fdf3573d1
Sandip Saha <sandipsmoto@gmail.com>
2017-09-04 18:54:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var connectionString = 'postgresql://sandipsmoto:db-sandipsmoto-10353@localhost:5432/sandipsmoto';
+var connectionString = 'postgresql://sandipsmoto:db-sandipsmoto-10353@database.localhost:5432/sandipsmoto';
 
 var pool = new Pool(connectionString);
 
