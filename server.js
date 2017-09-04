a6329657d53a70a75f1e2a6dacc4ca4eb98f56b9
Sandip Saha <sandipsmoto@gmail.com>
2017-09-04 19:05:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/test', function(req, res){
         if(err)
             res.status(500).send(err.toString());    
         else
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.row));
     });
 });
 
