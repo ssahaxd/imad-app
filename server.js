2290aea1c08246d533d91feab3e9295cc2d9484e
Sandip Saha <sandipsmoto@gmail.com>
2017-09-04 20:24:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,15 +26,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 /* DaraBase - test */
-app.get('/test', function(req, res){
-    // make a select command
-    pool.query('SELECT * FROM "test"', function (err, result){
-        if(err)
-            res.status(500).send(err.toString());    
-        else
-            res.send(JSON.stringify(result.rows));
-    });
-});
+
 
 
 /* Template function */
