6be075305409a9ec29f40eeb0937ef3e40ce90b6
Sandip Saha <sandipsmoto@gmail.com>
2017-09-09 00:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,22 +30,3 @@ button.onclick = function(){
     request.open('GET', 'http://sandipsmoto.imad.hasura-app.io/counter', true);
     request.send(null);
 };
-
-$(document).ready(function(){
-	var formInputs = $('input[type="email"],input[type="password"]');
-	formInputs.focus(function() {
-       $(this).parent().children('p.formLabel').addClass('formTop');
-       $('div#formWrapper').addClass('darken-bg');
-       $('div.logo').addClass('logo-active');
-	});
-	formInputs.focusout(function() {
-		if ($.trim($(this).val()).length == 0){
-		$(this).parent().children('p.formLabel').removeClass('formTop');
-		}
-		$('div#formWrapper').removeClass('darken-bg');
-		$('div.logo').removeClass('logo-active');
-	});
-	$('p.formLabel').click(function(){
-		 $(this).parent().children('.form-style').focus();
-	});
-});
